02f7ed87186fd7a2a5ad113fc677c54f31b30564
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2024-11-23 15:49:26 +0200

    [DEV] - Added main files for karate-auth-service

---
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/KarateAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.karate_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarateAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarateAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/unibuc/fmi/karate_auth_service/KarateAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.unibuc.fmi.karate_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarateAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
