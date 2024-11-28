32fc0883142eb12ba9da66124fc0bc77b9d1d707
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2024-11-28 16:40:56 +0200

    [DEV] - Create User

---
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package ro.unibuc.fmi.karate_auth_service.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
+import ro.unibuc.fmi.karate_auth_service.models.user.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+    Optional<User> findByEmail(@NonNull String email);
+}
