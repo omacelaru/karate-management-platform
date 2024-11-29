f1c0aca3c55fa8c9aeac1ddec080f93079bae717
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2024-11-29 14:38:31 +0200

    [DEV] - Added GlobalExceptionHandler

---
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package ro.unibuc.fmi.karate_auth_service.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiError> handleAccessDeniedException(AccessDeniedException error, WebRequest request) {
+        ApiError apiError = new ApiError(
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                error.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now().toString()
+        );
+        log.error("Access denied: {}", error.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(apiError);
+    }
+
+    @ExceptionHandler({BadCredentialsException.class, AuthenticationCredentialsNotFoundException.class})
+    public ResponseEntity<ApiError> handleBadCredentialsException(Exception error, WebRequest request) {
+        ApiError apiError = new ApiError(
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                error.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now().toString()
+        );
+        log.error("Unauthorized access: {}", error.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(Exception error, WebRequest request) {
+        ApiError apiError = new ApiError(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                //TODO - set a generic message to improve security after testing
+                error.getMessage(),
+                request.getDescription(false),
+                LocalDateTime.now().toString()
+        );
+        log.error("An error occurred: {}", error.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiError);
+    }
+
+}
