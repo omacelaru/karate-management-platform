02acc53d228bf08e758ea033dc8516b08accb6e8
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2024-11-29 14:44:54 +0200

    [IMPROVE] - Return logged user

--- src/main/java/ro/unibuc/fmi/karate_auth_service/services/UserService.java
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/services/UserService.java
@@ -1,7 +1,7 @@
 package ro.unibuc.fmi.karate_auth_service.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -10,16 +10,20 @@ import ro.unibuc.fmi.karate_auth_service.models.user.User;
 import ro.unibuc.fmi.karate_auth_service.repositories.UserRepository;
 import ro.unibuc.fmi.karate_auth_service.utils.MapperUtils;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final MapperUtils mapperUtils;
 
-    public UserResponse getMe() throws ChangeSetPersister.NotFoundException {
+    public UserResponse getMe() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String email = authentication.getName();
-        User user = userRepository.findByEmail(email).orElseThrow(ChangeSetPersister.NotFoundException::new);
-        return mapperUtils.mapToUserResponse(user);
+        log.info("Getting user with email: {}", email);
+        Optional<User> user = userRepository.findByEmail(email);
+        return user.map(mapperUtils::mapToUserResponse).orElse(null);
     }
 }
