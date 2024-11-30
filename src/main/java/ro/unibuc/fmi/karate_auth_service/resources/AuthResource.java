b97e266d43e789282063f156042daeb44a680f8b
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2024-11-30 12:55:13 +0200

    [FIX] - Remove unused import statement

--- src/main/java/ro/unibuc/fmi/karate_auth_service/resources/AuthResource.java
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/resources/AuthResource.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/ro/unibuc/fmi/karate_auth_service/services/AuthService.java
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/services/AuthService.java
@@ -1,7 +1,6 @@
 package ro.unibuc.fmi.karate_auth_service.services;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
