f86784155e4fa1ed48b6b7c61ca5a3c136a734dc
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2024-12-31 13:52:47 +0200

    [DEV] - Added mapper for club DTOs

--- src/main/java/ro/unibuc/fmi/karate_auth_service/utils/MapperUtils.java
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/utils/MapperUtils.java
@@ -5,10 +5,14 @@ import org.mapstruct.Mapper;
 import ro.unibuc.fmi.karate_auth_service.dtos.athlete.AthleteRequest;
 import ro.unibuc.fmi.karate_auth_service.dtos.athlete.AthleteResponse;
 import ro.unibuc.fmi.karate_auth_service.dtos.auth.AuthResponse;
+import ro.unibuc.fmi.karate_auth_service.dtos.club.ClubRequest;
+import ro.unibuc.fmi.karate_auth_service.dtos.club.ClubResponse;
+import ro.unibuc.fmi.karate_auth_service.dtos.club.ClubWithCoachesResponse;
 import ro.unibuc.fmi.karate_auth_service.dtos.coach.CoachRequest;
 import ro.unibuc.fmi.karate_auth_service.dtos.coach.CoachResponse;
 import ro.unibuc.fmi.karate_auth_service.dtos.user.UserResponse;
 import ro.unibuc.fmi.karate_auth_service.models.athelte.Athlete;
+import ro.unibuc.fmi.karate_auth_service.models.club.Club;
 import ro.unibuc.fmi.karate_auth_service.models.coach.Coach;
 import ro.unibuc.fmi.karate_auth_service.models.user.User;
 
@@ -22,10 +26,13 @@ public interface MapperUtils {
 
     Athlete mapToAthlete(AthleteRequest athleteRequest);
 
-    User toEntity(User user);
-
     CoachResponse mapToCoachResponse(Coach coach);
 
     Coach mapToCoach(@Valid CoachRequest coachRequest);
 
+    ClubWithCoachesResponse mapToClubWithCoachesResponse(Club club);
+
+    ClubResponse mapToClubResponse(Club club);
+
+    Club mapToClub(@Valid ClubRequest clubRequest);
 }
