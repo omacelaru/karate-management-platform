7c2b90c21bf9afcc2c527638aebbb8de7512fbcb
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2025-01-08 20:19:09 +0200

    [FIX] - Remove unused constructors

--- src/main/java/ro/unibuc/fmi/karate_auth_service/exceptions/RequestNotFoundException.java
+++ src/main/java/ro/unibuc/fmi/karate_auth_service/exceptions/RequestNotFoundException.java
@@ -4,12 +4,4 @@ public class RequestNotFoundException extends RuntimeException {
     public RequestNotFoundException() {
         super("Request not found or it is not in the pending status");
     }
-
-    public RequestNotFoundException(String message) {
-        super(message);
-    }
-
-    public RequestNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
