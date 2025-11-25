169e4eeb7c393c8fe22ca8ec2a2a834c32dfb22b
Macelaru Octavian <octavian-andrei.macelaru@s.unibuc.ro>
2025-11-25 20:35:47 +0200

    [ADD] - Implement MaxCategoriesPerKey validation and update application configuration

--- src/main/java/ro/unibuc/fmi/karate_management_platform/dtos/competition/registration/CompetitionRegistrationRequest.java
+++ src/main/java/ro/unibuc/fmi/karate_management_platform/dtos/competition/registration/CompetitionRegistrationRequest.java
@@ -1,7 +1,7 @@
 package ro.unibuc.fmi.karate_management_platform.dtos.competition.registration;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Size;
+import ro.unibuc.fmi.karate_management_platform.annotations.MaxCategoriesPerKey;
 import ro.unibuc.fmi.karate_management_platform.models.competition.category.individual.IndividualCategoryType;
 import ro.unibuc.fmi.karate_management_platform.models.competition.category.team.TeamCategoryType;
 
@@ -10,9 +10,11 @@ import java.util.Set;
 
 public record CompetitionRegistrationRequest(
         //TODO validate for more than 2 categories
+        @MaxCategoriesPerKey
         @Schema(description = "Set of individual categories the athlete is registered for", example = "{\"1\":[\"KATA_INDIVIDUAL\",\"KUMITE_INDIVIDUAL\"],\"2\":[\"KATA_INDIVIDUAL\",\"KUMITE_INDIVIDUAL\"],\"3\":[\"KATA_INDIVIDUAL\"]}")
         Map<Long, Set<IndividualCategoryType>> individualCategories,
 
+        @MaxCategoriesPerKey
         @Schema(description = "Map of team categories and associated team member IDs", example = "{\"1\":[\"KATA_TEAM\",\"KUMITE_TEAM\"],\"2\":[\"KATA_TEAM\",\"KUMITE_TEAM\"],\"3\":[\"KATA_TEAM\"]}")
         Map<Long, Set<TeamCategoryType>> teamCategories) {
 }
